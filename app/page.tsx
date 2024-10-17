63791c29a90c9b86e6ef39eee821c5b367799a25
Selcuk Cihan <selcukcihan@gmail.com>
2024-10-17 18:05:29 +0300

    Initial implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,89 @@
-import Image from "next/image";
+'use server'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { redirect } from 'next/navigation'
+import ogs from 'open-graph-scraper'
+import './globals.css'
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const checkUrl = (url: string) => {
+  try {
+    const urlObject = new URL(url)
+    return (urlObject.hostname === 'open.substack.com') ? true : false
+  } catch (error) {
+    return false
+  }
+}
+
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const url = searchParams.url as string
+  console.log('Fetching metadata from', url)
+
+  if (!checkUrl(url)) {
+    return {}
+  } else {
+    try {
+      const { error, html, result, response } = await ogs({url})
+      if (error) {
+        console.error('Error fetching metadata', error, result)
+        return {}
+      }
+      const title = result.ogTitle
+      const metaDescription = result.ogDescription
+      const images = [{
+        url: result.twitterImage?.[0].url,
+        width: 360,
+        height: 360,
+        alt: metaDescription,
+      }]
+      return {
+        title: title,
+        description: metaDescription,
+        applicationName: title,
+        keywords: ['books', 'reading', 'bookshelf', 'personal', 'list', 'notes', 'ratings', 'library', 'bookworm', 'booklover', 'kindle', 'goodreads', 'bookclub', 'bookstore', 'bookshop', 'bookcase', 'bookstack', 'bookshelfie', 'bookstagram', 'bookphotography', 'bookcommunity', 'bookaddict', 'bookaholic', 'booknerd', 'bookgeek', 'bookish', 'bookdragon', 'bookworms', 'booklovers', 'bookclubs', 'bookstores', 'bookshops', 'bookcases', 'bookstacks', 'bookshelves', 'bookstagrammer', 'bookphotographer', 'bookcommunitys', 'bookaddicts', 'bookaholics', 'booknerds', 'bookgeeks', 'bookishs', 'bookdragons'],
+        twitter: {
+          card: 'summary',
+          title: title,
+          description: metaDescription,
+          creator: '@scihan',
+          site: '@scihan',
+          images,
+        },
+        openGraph: {
+          siteName: title,
+          title: title,
+          type: 'website',
+          url: result.ogUrl,
+          description: metaDescription,
+          images,
+        },
+      }
+    } catch (error) {
+      console.error('Error fetching metadata', error)
+      return {}
+    }
+  }
+}
+
+// This page receives a substack post link as query parameter.
+// It fetches the URL and displays the opengraph metadata from that post.
+// It then redirects to the URL.
+// Example url: https://open.substack.com/pub/scihan/p/kitap-ozeti-made-to-stick?r=2f5dx&utm_campaign=post&utm_medium=web
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const url = searchParams.url as string
+  console.log('Redirecting to', url)
+  if (!checkUrl(url)) {
+    // return 404 status
+    redirect('/not-found')
+  } else {
+    // redirect to the url
+    redirect(url + '&utm_campaign=post&utm_medium=web')
+  }
 }
